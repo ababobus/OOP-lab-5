0c83bb40d55cc06b8a384375d09c6f3eaceff75e
Азалия Баймуратова <ozalea@yandex.ru>
2024-04-30 01:33:49 +0500

    fixed constructor in Base

--- Base-Desc.cpp
+++ Base-Desc.cpp
@@ -3,41 +3,32 @@
 
 class Base {
 private:
-    int num = 0;
+    int value = 0;
 public:
-    void set_num(int num) {
-        this->num = num;
+    void set_value(int value) {
+        this->value = value;
     }
-    int get_num() {
-        return this->num;
-    }
-    Base(int num) {
-        this->num = num;
-        std::cout << "Base(int num) constructor worked\n";
-    }
-
     Base() {
         std::cout << "Base constructor worked\n";
     }
     Base(const Base* obj) {
+        this->value = obj->value;
         std::cout << "Base(Base*obj) worked\n";
     }
     Base(const Base& obj) {
+        this->value = obj.value;
         std::cout << "Base(Base &obj) worked\n";
     }
     virtual ~Base() {
         std::cout << "virtual ~Base() worked\n";
     }
+    virtual void get_value() {
+        std::cout << "Base virtual get value worked, this->value: " << this->value << "\n";
+    }
 };
 
 class Desc : public Base {
-private:
-    int value = 0;
 public:
-    Desc(int value) {
-        this->value = value;
-    }
-
     Desc() {
         std::cout << "Decs constructor worked\n ";
     }
@@ -45,57 +36,71 @@ public:
         std::cout << "Desc(Desc *obj) worked\n";
     }
 
-    Desc(Desc& obj) {   
+    Desc(Desc& obj) {  
         std::cout << "Desc(Desc &obj) worked\n";
     }
 
     virtual ~Desc() override {
-        std::cout << "~Desc() override worked\n";
+        std::cout << "virtual~Desc() override worked\n";
+    }
+    virtual void get_value() override {
+        std::cout << "Desc virtual get value worked\n";
     }
 };
 
 void func1(Base obj) {
-    obj.set_num(obj.get_num() + 1);
+    obj.get_value();
     std::cout << "func1(Base *obj) worked\n";
 }
 
 void func2(Base* obj) {
-    obj->set_num(obj->get_num()+1);
-    std::cout << "func2(Base *obj) worked\n";
+    //obj.get_value();
+    if (dynamic_cast<Desc*>(obj)) {
+        std::cout << "func2(Base *obj) with derived Desc object worked\n";
+    }
+    else {
+        std::cout << "func2(Base *obj) with non-derived object worked\n";
+    }
 }
 
 void func3(Base& obj) {
-    obj.set_num(obj.get_num() + 1);
-    std::cout << "func3(Base &obj) worked\n";
+    //obj.get_value();
+    if (dynamic_cast<Desc*>(&obj)) {
+        std::cout << "func3(Base &obj) with derived Desc object worked\n";
+    }
+    else {
+        std::cout << "func3(Base &obj) with non-derived object worked\n";
+    }
 }
 
-Base func1() {
-    Base o1 = func1();
+Base func1_static() {//переполнение стека 
+    Base o1;
     std::cout << "Base func1() static \n";
     return o1;
-
 };
-Base* func2() { //UB
-    Base o2 = func2();
+
+Base* func2_static() { //UB
+    Base o2;
     std::cout << "Base func2() static \n";
     return &o2;
 };
-Base& func3() {//UB
-    Base o3 = func3();
+
+Base& func3_static() {//UB
+    Base o3;
     std::cout << "Base func3() static \n";
     return o3;
 };
-Base func4() { //утечка памяти
+Base func4_dynamic() { //утечка памяти
     Base* o4 = new Base();
     std::cout << "Base func4() dynamic \n";
     return *o4;
 };
-Base* func5() { //мб утечка памяти, нунжны умные указатели
+Base* func5_dynamic() { //мб утечка памяти, нужны умные указатели
     Base* o5 = new Base();
     std::cout << "Base func5() dynamic \n";
     return o5;
 };
-Base& func6() { //утечка памяти, надо делете
+Base& func6_dynamic() { //утечка памяти, надо делет
     Base* o6 = new Base();
     std::cout << "Base func6() dynamic \n";
     return *o6;
@@ -105,19 +110,48 @@ Base& func6() { //утечка памяти, надо делете
 
 int main()
 {
-    Base* basePrimer = new Base(10);
-    Desc* descPrimer = new Desc(5);
+    Base basePrimer;
+    Desc descPrimer;
+
+    basePrimer.set_value(5);
+    descPrimer.set_value(9);
 
     func1(basePrimer);
     func1(descPrimer);
 
-    func2(basePrimer);
-    func2(descPrimer);
+    func2(&basePrimer);
+    func2(&descPrimer);
 
-    func3(*basePrimer);
-    func3(*descPrimer);
+    func3(basePrimer);
+    func3(descPrimer);
 
-    //descPrimer->Base::get_num();
+    //descPrimer->Base::get_value();
+
+    Base o1 = func1_static(); 
+    o1.set_value(3);
+    o1.get_value();
+
+    Base* o2 = func2_static();
+    //o2->get_value();
+    //delete o2; объект удалился в функции, удалять нечего
+
+
+    Base &o3 = func3_static(); //возвращает ссылку на локальную переменную внутри функции
+    //o3.get_value();
+    //delete &o3; объект удалился в функции, удалять нечего
+
+    Base o4 = func4_dynamic();
+    o4.set_value(99);
+    o4.get_value(); //не удалится в функции
+
+    Base* o5 = func5_dynamic();
+    o5->set_value(8);
+    delete o5;
+
+    Base& o6 = func6_dynamic();
+    o6.set_value(34);
+    o6.get_value();
+    delete& o6;
 
     std::cout << "Hello World!\n";
 }
