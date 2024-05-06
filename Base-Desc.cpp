dcbf6d88e7684bf755141b9a5cf81c16a3bf3028
Азалия Баймуратова <ozalea@yandex.ru>
2024-05-06 17:42:49 +0500

    done base desc

--- Base-Desc.cpp
+++ Base-Desc.cpp
@@ -13,11 +13,11 @@ public:
     }
     Base(const Base* obj) {
         this->value = obj->value;
-        std::cout << "Base(Base*obj) worked\n";
+        std::cout << "Base(const Base*obj) worked\n";
     }
     Base(const Base& obj) {
         this->value = obj.value;
-        std::cout << "Base(Base &obj) worked\n";
+        std::cout << "Base(const Base &obj) worked\n";
     }
     virtual ~Base() {
         std::cout << "virtual ~Base() worked\n";
@@ -46,61 +46,67 @@ public:
     virtual void get_value() override {
         std::cout << "Desc virtual get value worked\n";
     }
+    void some_desc_method() {
+        std::cout << "Desc::some method worked\n";
+    }
 };
 
 void func1(Base obj) {
     obj.get_value();
-    std::cout << "func1(Base *obj) worked\n";
+    std::cout << __LINE__ << " " << "func1(Base * obj) worked\n";
 }
 
 void func2(Base* obj) {
-    //obj.get_value();
-    if (dynamic_cast<Desc*>(obj)) {
-        std::cout << "func2(Base *obj) with derived Desc object worked\n";
+    //obj->get_value();
+    //obj->some_desc_method(); не сработает, можно привести тип
+    Desc* c = dynamic_cast<Desc*>(obj);
+    if (c != nullptr){ //действительно ли хранится по адресу объект Desc* ?
+        c->some_desc_method();
+        std::cout << __LINE__ << " " << "func2(Base *obj) with derived Desc object worked\n";
     }
     else {
-        std::cout << "func2(Base *obj) with non-derived object worked\n";
+        std::cout << __LINE__ << " " << "func2(Base *obj) with non-derived object worked\n";
     }
 }
 
-void func3(Base& obj) {
-    //obj.get_value();
-    if (dynamic_cast<Desc*>(&obj)) {
-        std::cout << "func3(Base &obj) with derived Desc object worked\n";
+void func3(Base &obj) { 
+    Desc* d = dynamic_cast<Desc*>(&obj);
+    if (d != nullptr) {
+        d->some_desc_method();
+        std::cout << __LINE__ << " " << "func3(Base &obj) with derived Desc object worked\n";
     }
     else {
-        std::cout << "func3(Base &obj) with non-derived object worked\n";
+        std::cout << __LINE__ << " " << "func3(Base &obj) with non-derived object worked\n";
     }
 }
 
-Base func1_static() {//переполнение стека 
+Base func1_static() {
     Base o1;
     std::cout << "Base func1() static \n";
     return o1;
 };
 
-Base* func2_static() { //UB
+Base* func2_static() { 
     Base o2;
     std::cout << "Base func2() static \n";
     return &o2;
 };
-
-Base& func3_static() {//UB
+Base& func3_static() {
     Base o3;
     std::cout << "Base func3() static \n";
     return o3;
 };
-Base func4_dynamic() { //утечка памяти
+Base func4_dynamic() { 
     Base* o4 = new Base();
     std::cout << "Base func4() dynamic \n";
     return *o4;
 };
-Base* func5_dynamic() { //мб утечка памяти, нужны умные указатели
+Base* func5_dynamic() { 
     Base* o5 = new Base();
     std::cout << "Base func5() dynamic \n";
     return o5;
 };
-Base& func6_dynamic() { //утечка памяти, надо делет
+Base& func6_dynamic() { 
     Base* o6 = new Base();
     std::cout << "Base func6() dynamic \n";
     return *o6;
@@ -109,49 +115,54 @@ Base& func6_dynamic() { //утечка памяти, надо делет
 
 
 int main()
-{
+{    
     Base basePrimer;
-    Desc descPrimer;
-
-    basePrimer.set_value(5);
-    descPrimer.set_value(9);
-
+    Desc descPrimer; //вызовется конструктор Base и Desc  
+    Base* r = new Desc(); //неявный конструктор Base и конструктор Desc 
+    //Desc::Desc(); можно так вызвать конкретный конструктор
+    
+    std::cout << "\n";
+    std::cout << "3 void functions:\n";
     func1(basePrimer);
-    func1(descPrimer);
+    func1(descPrimer); //передаем копию desc типа base
 
-    func2(&basePrimer);
+    func2(&basePrimer); //basePrimer не является объектом-потомком Desc
     func2(&descPrimer);
+    func2(r); //r типа Base*, указывает на Desc
 
     func3(basePrimer);
     func3(descPrimer);
 
-    //descPrimer->Base::get_value();
+    std::cout<<"\n";
+    //r->Base::get_value();
 
-    Base o1 = func1_static(); 
+    std::cout << "6 Base*& functions:\n";
+    Base o1 = func1_static();  
     o1.set_value(3);
     o1.get_value();
 
     Base* o2 = func2_static();
-    //o2->get_value();
-    //delete o2; объект удалился в функции, удалять нечего
-
+    o2->get_value();
+    //delete o2; UB объект удалился в функции, удалять нечего
 
     Base &o3 = func3_static(); //возвращает ссылку на локальную переменную внутри функции
-    //o3.get_value();
-    //delete &o3; объект удалился в функции, удалять нечего
+    o3.get_value();
+    //delete &o3; UB объект удалился в функции, удалять нечего
 
-    Base o4 = func4_dynamic();
+    Base o4 = func4_dynamic();//локальный объект не удалится в функции, висит в памяти
     o4.set_value(99);
-    o4.get_value(); //не удалится в функции
+    o4.get_value(); 
 
-    Base* o5 = func5_dynamic();
+    Base* o5 = func5_dynamic(); //мб утечка памяти
     o5->set_value(8);
     delete o5;
 
-    Base& o6 = func6_dynamic();
+    Base& o6 = func6_dynamic(); //утечка памяти, надо delete&
     o6.set_value(34);
     o6.get_value();
     delete& o6;
 
     std::cout << "Hello World!\n";
+
+    delete r;
 }
